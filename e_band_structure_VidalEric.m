b4de41e99d900a32d1ff184f3bb3f6ec72dc9425
Eric Vidal <ericvidalmarcos@gmail.com>
2024-01-19 00:59:16 +0100

    everything working

--- e_band_structure_VidalEric.m
+++ e_band_structure_VidalEric.m
@@ -25,7 +25,7 @@ g(1:3, 1)=cross(a(:,1), a(:,2)) / cell_volume;
 g(1:3, 2)=cross(a(:,2), a(:,3)) / cell_volume;
 g(1:3, 3)=cross(a(:,3), a(:,1)) / cell_volume;
 for i=1:3
-    g(4, i)=g(1:3, i)' * g(1:3, i);   % Squared norm of each vector
+  g(4, :)=vecnorm(g(1:3, :));   % Squared norm of each vector
 end
 
 %% RECIPROCAL LATTICE VECTORS
@@ -39,7 +39,7 @@ for i=-nstep:nstep
     for k=-nstep:nstep
       n=n+1;
       G(1:3,n)=i*g(1:3,1) + j*g(1:3,2) + k*g(1:3,3);
-      G(4,n)=G(1:3,n)' * G(1:3,n);  % Squared norm of each vector
+      G(4,n)=sum(G(1:3,n).^2);  % Squared norm of each vector
     end
   end
 end
@@ -48,7 +48,7 @@ G=G(:, G(4,:) <= cutoff);  % Remove G vectors with norm > cutoff
 [~,Gcut]=size(G);  % Number of G vectors fulfilling the cutoff
 
 %% COMPUTE THE PSEUDOPOTENTIALS
-cvg=zeros(Gcut,1);
+Vg=zeros(Gcut,1);
 cases = [0, 3, 4, 8, 11];  % The cases you want to handle
 for Gnum = find(ismember(G(4,:), cases))
   switch G(4,Gnum)
@@ -68,118 +68,61 @@ for Gnum = find(ismember(G(4,:), cases))
       sym=ff(m,4);
       asym=ff(m,7);
   end
-  argu=2*pi * (G(1:3,Gnum)' * tau(1:3,1));
-  cvg(Gnum)=(cos(argu)*sym - 1i*sin(argu)*asym) * Rydberg;
+  G_s=2*pi * G(1:3,Gnum)' * tau(1:3,1);
+  Vg(Gnum)=(cos(G_s)*sym - 1i*sin(G_s)*asym) * Rydberg;   % (eq 3.107)
 end
 
-toc
-
-%% COMPUTE THE BZ PATH AND HAMILTONIAN
-[q,tix,til]=BZpath(BZstep,qs,qe,qs_str,qe_str);
-[~,nq]=size(q);
-
-H=zeros(Gcut,Gcut);
-G_diff=zeros(5,1);
+%% COMPUTE THE HAMILTONIAN FULFILLING GS_MAX
+% CONDITION: |G|^2 of highest non zero Fourier coefficients
+% in expanding potential [2*pi/spacing]^2
+Ham=zeros(Gcut,Gcut); % Hamiltonian matrix (eq 3.20)
+G_aux=zeros(4,1);     % Auxiliar vector
 for jj=1:Gcut
   for ii=1:Gcut
-    G_diff(1:3)=G(1:3,ii)-G(1:3,jj);
-    G_diff(5)=G_diff(1:3)'*G_diff(1:3);
-    if(G_diff(5)<=Gs_max)
-      diff_norm = vecnorm(G_diff(1:3)-G(1:3,:), 2, 1);
+    G_aux(1:3)=G(1:3,ii)-G(1:3,jj);
+    G_aux(4)=sum(G_aux(1:3).^2);
+    if(G_aux(4)<=Gs_max)
+      diff_norm = vecnorm(G_aux(1:3)-G(1:3,:));
       kk = find(abs(diff_norm) < tol);
       if ~isempty(kk)
-        H(ii,jj)=cvg(kk(1));
+        Ham(ii,jj)=Vg(kk(1));
       end
     end
   end
 end
 
-for iq=1:nq
-  testo(iq)=iq;
-  %%Kinetic energy
+%% COMPUTE THE BZ PATH
+[qpath,tix,til]=BZpath(BZstep,qs,qe,qs_str,qe_str);
+[~,nqpath]=size(qpath);
+
+%% COMPUTE THE BAND STRUCTURE
+for qq=1:nqpath
   for ii=1:Gcut
-    for jj=1:3
-      p(jj)=q(jj,iq)-G(jj,ii);
+    for jj=1:3  % qpath: Cartesian coordinates of wavevector i
+      p(jj)=qpath(jj,qq)-G(jj,ii);
     end
-    H(ii,ii)=ekinunit*(p*p')+cvg(1);  % Hamiltonian diagonal
+    Ham(ii,ii)=ekinunit*p*p'+Vg(1);  % Diagonal
   end
-  [v,ev]=eig(H);
-  E=real(diag(ev));
-  [E,perm]=sort(E);
-  v=v(:,perm);
-  for ii=1:nband
-        firstEs(ii,iq)=E(ii);
+  [~,D]=eig(Ham);     % Diagonalization
+  eigenvals=diag(D);  % Extract eigenvalues
+  for kk=1:nband
+    Eband(kk,qq)=eigenvals(kk);
   end
-
-end
-
-
-for ii=1:nq
-  separ(ii)=20*((-1)^q(6,ii)); %Function used to later put a line to separate paths
 end
 % Print the last value until 7th decimal
-fprintf('%.9f\n',firstEs(4,27))
-plot(q(5,:),firstEs,'-o','color', 'black','MarkerSize', 1, 'linewidth', 0.5,q(5,:),separ,'color','red');
-ylabel('E(eV)','FontSize',18);
-xlim([0,q(5,nq)]);
-if(m==1)
-  ylim([-5.5,6]);
-  title ("Band structure for Si", "fontsize", 20);
-  set(gca, 'ytick', -5:1:6);
-elseif(m==2)
-  ylim([-5,7]);
-  title ("Band structure for Ge", "fontsize", 20);
-  set(gca, 'ytick', -5:1:7);
-elseif(m==3)
-  ylim([-4,6]);
-  title ("Band structure for Sn", "fontsize", 20);
-  set(gca, 'ytick', -4:1:6);
-elseif(m==4)
-  ylim([-4,7]);
-  title ("Band structure for GaP", "fontsize", 20);
-  set(gca, 'ytick', -4:1:7);
-elseif(m==5)
-  ylim([-4,7]);
-  title ("Band structure for GaAs", "fontsize", 20);
-  set(gca, 'ytick', -4:1:7);
-elseif(m==6)
-  ylim([-4,7]);
-  title ("Band structure for AlSb", "fontsize", 20);
-  set(gca, 'ytick', -4:1:7);
-elseif(m==7)
-  ylim([-4,7]);
-  title ("Band structure for InP", "fontsize", 20);
-  set(gca, 'ytick', -4:1:7);
-elseif(m==8)
-  ylim([-3,6.4]);
-  title ("Band structure for GaSb", "fontsize", 20);
-  set(gca, 'ytick', -3:1:6);
-elseif(m==9)
-  ylim([-4,7]);
-  title ("Band structure for InAs", "fontsize", 20);
-  set(gca, 'ytick', -4:1:7);
-elseif(m==10)
-  ylim([-3,6]);
-  title ("Band structure for InSb", "fontsize", 20);
-  set(gca, 'ytick', -3:1:6);
-elseif(m==11)
-  ylim([-3,10]);
-  title ("Band structure for ZnS", "fontsize", 20);
-  set(gca, 'ytick', -3:1:10);
-elseif(m==12)
-  ylim([-3,9]);
-  title ("Band structure for ZnSe", "fontsize", 20);
-  set(gca, 'ytick', -3:1:9);
-elseif(m==13)
-  ylim([-3,8.5]);
-  title ("Band structure for ZnTe", "fontsize", 20);
-  set(gca, 'ytick', -3:1:8);
-elseif(m==14)
-  ylim([-3.5,8]);
-  title ("Band structure for CdTe", "fontsize", 20);
-  set(gca, 'ytick', -3:1:8);
+fprintf('%.9f\n',Eband(4,27))
+
+%% WRITE THE BAND STRUCTURE IN A FILE
+filename=strcat(int2str(m), 'bandstructure.dat');
+if(dispersion_relation)
+  fid=fopen(filename,'w');
+  for ii=1:nqpath
+    for jj=1:nband
+      fprintf(fid,'%f %f\n',qpath(5,ii),Eband(jj,ii));
+    end
+    fprintf(fid,'\n');
+  end
+  fclose(fid);
 end
 
-set (gca,'xtick',tix);
-set (gca,'xticklabel',til,'FontSize',18,'FontWeight','bold');
-
+toc
